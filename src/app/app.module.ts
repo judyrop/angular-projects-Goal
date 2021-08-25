37dd62463edfd937858d25e0dd5589a3fb93b05e
Your Name <youremail@domain.com>
2021-08-25 17:54:33 +0300

    add the changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { GoalComponent } from './goal/goal.component';
+// import { Goal } from './goal';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { GoalService } from './goal-service/goal.service';
+
+// import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 @NgModule({
   declarations: [
-    // AppComponent,
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent,
+  
     // GoalComponent
   
     // GoalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [GoalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,23 +1,38 @@
-// import { Component, OnInit } from '@angular/core';
-// import {Goal} from '../goal';
-// @Component({
-//   selector: 'app-goal',
-//   templateUrl: './goal.component.html',
-//   styleUrls: ['./goal.component.css']
-// })
-// export class GoalComponent implements OnInit {
-// goals:Goal[]=[
-//   {id:1, name :'Watching finding Nemo'},
-//   {id:2, name :'Buy Cookies'},
-//   {id:3, name :'Get new Phone Case'},
-//   {id:4, name :'Get Dog Food'},
-//   {id:5, name :'Solve math homework'},
-//   {id:6, name :'Plot my world domination plan'},
-  
-// ];
-//   constructor() { }
+import { Component, OnInit } from '@angular/core';
+import {Goal} from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+  goals:Goal[];
 
-//   ngOnInit(): void {
-//   }
+  constructor(goalService:GoalService) {
+    this.goals = goalService.getGoals()
+  }
+  addNewGoal(goal:any){
+    let goalLength= this.goals.length;
+    goal.id = goalLength+1;
+    goal.completedate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+  toggleDetails(index:any){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  deleteGoal(isComplete:boolean,index:number){
+  if (isComplete) {
+    let toDelete = confirm('Are you sure you want to delete ${this.goals[index].name}?')
+ 
+    if(toDelete){
+      this.goals.splice(index,1);
+    }
+  }
+  }
+ 
 
-// }
+  ngOnInit(): void {
+  }
+
+}
